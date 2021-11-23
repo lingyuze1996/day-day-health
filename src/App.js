4766d858fe7bf24e7f5648f9f0c3ecc8d258720d
lingyuze1996 <62694056+lingyuze1996@users.noreply.github.com>
2021-11-23 21:23:36 +1100

    Improve Authentication Workflow

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import { authToken } from './apis/AuthAPI';
+import { authCode, authRefreshToken } from './apis/AuthAPI';
 import './App.css';
 import Tracker from './components/Tracker';
 
 
 function App() {
+  const AUTH_URL = "https://auth.daydayhealth.cf/login?response_type=code&client_id=1ulb350s6u8a4efebc44la1ku4&redirect_uri=https://www.daydayhealth.cf"
 
   // Try to get code from path query string parameters
-  let regex = /#id_token=(.*)&access_token=/
-  let token = window.location.href.match(regex)?.[1]
+  let regex = /\?code=(.*)$/
+  let code = window.location.href.match(regex)?.[1]
 
-  // alert(token)
+  if (code !== undefined) {
+    authCode(code)
+      .then((data) => {
+        if (data.error) {
+          window.location.href = AUTH_URL
+        } else {
+          localStorage.setItem("refreshToken", data["refresh_token"])
+          localStorage.setItem("idToken", data["id_token"])
+        }
+      })
+      .catch(e => {
+        console.log("Code Auth Error", e)
+        window.location.href = AUTH_URL
+      })
+  } else {
+    // Try to fetch refresh token from local storage
+    let refreshToken = localStorage.getItem("refreshToken")
 
-  // Try to fetch code from local storage
-  if (token === undefined) {
-    token = localStorage.getItem("token")
+    if (refreshToken !== null) {
+      authRefreshToken(refreshToken).then((data) => {
+        if (data.error) {
+          window.location.href = AUTH_URL
+        } else {
+          localStorage.setItem("idToken", data["id_token"])
+        }
+      })
+    } else {
+      window.location.href = AUTH_URL
+    }
   }
-
-  // No Code Provided
-  if (token === undefined || token === null) {
-    window.location.href = "https://auth.daydayhealth.cf/login?response_type=token&client_id=1ulb350s6u8a4efebc44la1ku4&redirect_uri=https://www.daydayhealth.cf"
-  }
-
-  authToken(token)
-    .then(claim => {
-      if (claim["cognito:username"]) {
-        localStorage.setItem("token", token)
-      } else {
-        window.location.href = "https://auth.daydayhealth.cf/login?response_type=token&client_id=1ulb350s6u8a4efebc44la1ku4&redirect_uri=https://www.daydayhealth.cf"
-      }
-    })
-    .catch(e => { 
-      console.log("Token Auth Error", e)
-      window.location.href = "https://auth.daydayhealth.cf/login?response_type=token&client_id=1ulb350s6u8a4efebc44la1ku4&redirect_uri=https://www.daydayhealth.cf" 
-    })
 
   return (
     <>
--- src/apis/AuthAPI.js
+++ src/apis/AuthAPI.js
@@ -1,12 +1,25 @@
-const TOKEN_AUTH_API = "https://api.daydayhealth.cf/auth"
+import axios from "axios"
 
-const authToken = async(token) => {
-    const authResponse = await fetch(TOKEN_AUTH_API, {
-        method: "POST",
-        body: JSON.stringify({ token })
-    })
+const TOKEN_AUTH_API = "https://auth.daydayhealth.cf/oauth2/token"
+const REDIRECT_URI = "https://www.daydayhealth.cf"
+const CLIENT_ID = "1ulb350s6u8a4efebc44la1ku4"
 
-    return await authResponse.json()
+const authCode = async (code) => {
+    const params = new URLSearchParams()
+    params.append("redirect_uri", REDIRECT_URI)
+    params.append("grant_type", "authorization_code")
+    params.append("client_id", CLIENT_ID)
+    params.append("code", code)
+    return (await axios.post(TOKEN_AUTH_API, params)).data;
 }
 
-export { authToken }
+const authRefreshToken = async (token) => {
+    const params = new URLSearchParams()
+    params.append("redirect_uri", REDIRECT_URI)
+    params.append("grant_type", "refresh_token")
+    params.append("client_id", CLIENT_ID)
+    params.append("refresh_token", token)
+    return (await axios.post(TOKEN_AUTH_API, params)).data;
+}
+
+export { authRefreshToken, authCode }
